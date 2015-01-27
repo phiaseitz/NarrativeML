f96899e73dbce0c41e5b1becf657e87648e3d2fc
phiaseitz <sophia.seitz@students.olin.edu>
2015-01-27 17:35:51 -0500

    Investigating the speaker changes

--- makexmlnarratives.py
+++ makexmlnarratives.py
@@ -3,7 +3,9 @@ import re
 import lxml
 
 
-def readFile (folder_path, num_narrative):
+def readFile (num_narrative):
+	#Where the folder is
+	folder_path = '/Volumes/Research/Adler Research/Sophia OSS Stuff/Narratives_txt/'
 	#Create the file name for each narratve and get the full path
 	file_name = 'FLSA_{narrative_num}.txt'.format(narrative_num = str(num_narrative).zfill(3))
 	file_path = folder_path + file_name
@@ -20,7 +22,17 @@ def readFile (folder_path, num_narrative):
 		return []
 
 
-def linesToSpeaker (narrative_lines, scenes, interviewer, participant):
+def linesToXML (narrative_lines):
+	#All the scenes
+	scenes = ['HIGH POINT', 'LOW POINT', 'TURNING POINT']
+
+	#Possible interviewier and participant tags in the txt files
+	interviewer = ['I', 'THE INTERVIEWER', 'INTERVIEWER']
+	participant = ['R', 'RESPONDENT' ,'MALE SPEAKER', 'FEMALE SPEAKER', 'INTERVIEWEE', 'THE INTERVIEWEE', 'PARTICIPANT', 'ANSWER']
+	
+	current_scene = ''
+	current_speaker = ''
+
 	for line in narrative_lines:
 		#Get rid of new lines and carriage returns
 		line = line.rstrip()
@@ -30,40 +42,30 @@ def linesToSpeaker (narrative_lines, scenes, interviewer, participant):
 
 		#If this is a scene heading
 		if line.upper() in scenes:
-			print ('scene is ' + line)
+			current_scene = line
+			print (current_scene)
 		#if there's no colon then we dont know who is speaking
 		elif colon_index == -1:
-			print ('speaker is not indicated')
+			print ('speaker remains the same: {speaker}'.format(speaker = current_speaker))
 		#If there's a colon early on -- we know we have a speaker
 		elif colon_index < 30:
-			speaker = line[:colon_index]
-			if not(speaker in interviewer) and not(speaker in participant):
-				print (speaker)
+			current_speaker = line[:colon_index]
+			print (current_speaker)
 
 
 def main():
-	#All the scenes
-	scenes = ['HIGH POINT', 'LOW POINT', 'TURNING POINT']
-
-	#Possible interviewier and participant tags in the txt files
-	interviewer = ['I', 'THE INTERVIEWER', 'INTERVIEWER']
-	participant = ['R', 'RESPONDENT' ,'MALE SPEAKER', 'FEMALE SPEAKER', 'INTERVIEWEE', 'THE INTERVIEWEE', 'PARTICIPANT', 'ANSWER']
-
 	#Narratives to start and end at
 	first_narrative = 1
 	last_narrative = 10
-
-	#Where the folder is
-	folder_path = '/Volumes/Research/Adler Research/Sophia OSS Stuff/Narratives_txt/'
 
 		#Loop through all the narratives
 	for narrative_number in range(first_narrative,last_narrative):
 		print ('reading narrative {narrative_num}'.format(narrative_num = str(narrative_number)))
 
-		narrative_text = readFile(folder_path,narrative_number)
+		narrative_text = readFile(narrative_number)
 
 		if narrative_text:
-			linesToSpeaker(narrative_text, scenes, interviewer, participant)
+			linesToXML(narrative_text)
 
 
 if __name__ == '__main__':
