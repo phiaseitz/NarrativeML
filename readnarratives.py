d43723eb09ebdb7a2687678e9069e6267f7038fb
phiaseitz <sophia.seitz@students.olin.edu>
2015-02-05 19:13:23 -0500

    Need to handle the case that a file does not exist

--- readnarratives.py
+++ readnarratives.py
@@ -1,7 +1,4 @@
-import os
-import re
 import string
-import csv
 from lxml import etree
 
 def readNarrativeFile(narrative_number):
@@ -20,7 +17,8 @@ def readNarrativeFile(narrative_number):
 def getOnlyResponses(narrative_xml):
 	root = narrative_xml.getroot()
 	root_attributes = root.attrib
-	responses_xml = etree.Element("narrative", narrative_num = root_attributes['narrative_num'])
+	responses_xml = etree.Element("narrative", 
+		narrative_num = root_attributes['narrative_num'])
 	for scene in root:
 		scene_attributes = scene.attrib
 		responses_xml.append(etree.Element('scene', 
@@ -38,19 +36,46 @@ def getOnlyResponses(narrative_xml):
 	print(etree.tostring(responses_xml, pretty_print=True))
 	return responses_xml
 
-
-def main():
-	#Narratives to start and end at
-	first_narrative = 1
-	last_narrative = 2
-
+def getAllResponses(first_narrative, last_narrative):
+	responses = []
 	#Loop through all the narratives
 	for narrative_number in range(first_narrative,last_narrative + 1):
-		print ('reading narrative {narrative_num}'.format(
-			narrative_num = str(narrative_number)))
 		#Read the narrative
 		narrative_xml = readNarrativeFile(narrative_number)
 		responses_xml = getOnlyResponses(narrative_xml)
+		responses.append(responses_xml)
+	return responses
+def xmlToList(xml_response, coding_dimension):
+	#root = xml_response.getroot()
+	score_key = 'scene_'+coding_dimension
+	narrative_responses = []
+	for scene in xml_response:
+		scene_attributes = scene.attrib
+		narrative_responses.append((scene.text,
+			int(scene_attributes[score_key])))
+	return narrative_responses
+
+def LoadNarrativeData(coding_dimension, first = 1, last = 164):
+	all_responses_xml = getAllResponses(first,last)
+	data = []
+	for response in all_responses_xml:
+		response_data = xmlToList(response, coding_dimension)
+		data = data + response_data
+	return data
+
+def main():
+	# #Narratives to start and end at
+	# first_narrative = 1
+	# last_narrative = 2
+
+	# #Loop through all the narratives
+	# for narrative_number in range(first_narrative,last_narrative + 1):
+	# 	print ('reading narrative {narrative_num}'.format(
+	# 		narrative_num = str(narrative_number)))
+	# 	#Read the narrative
+	# 	narrative_xml = readNarrativeFile(narrative_number)
+	# 	responses_xml = getOnlyResponses(narrative_xml)
+	print(LoadNarrativeData('communion'))
 
 if __name__ == '__main__':
 	main()
