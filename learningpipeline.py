6724776db9732426fcdc3dc51c331e9e9a175af1
phiaseitz <sophia.seitz@students.olin.edu>
2015-02-15 17:51:33 -0500

    Investigating bag of words models

--- learningpipeline.py
+++ learningpipeline.py
@@ -1,44 +1,97 @@
 import readnarratives
 import numpy
-import re
+import random
+import scipy
+import math
 
-from sklearn.cross_validation import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 
-def tokenize(texts):
-	count_vect = CountVectorizer()
-	tokenized_texts = count_vect.fit_transform(texts)
+from sklearn.linear_model import SGDClassifier
 
-	#print tokenized_texts.shape
-
-	tfidf_transformer = TfidfTransformer()
-	texts_tfidf = tfidf_transformer.fit_transform(tokenized_texts)
-
-	return texts_tfidf,count_vect
-
-def mostInformativeFeatures(classifier, vectorizer, categories):
+def mostInformativeFeatures(classifier, vectorizer, categories,number_of_features):
 	feature_names = numpy.asarray(vectorizer.get_feature_names())
 	for i, category in enumerate(categories):
-		top10 = numpy.argsort(classifier.coef_[i])[-10:]
-		print("%s: %s" % (category, " ".join(feature_names[top10])))
+		top = numpy.argsort(classifier.coef_[i])[-number_of_features:]
+		print("%s: %s" % (category, " ".join(feature_names[top])))
+
+
+def splitTestTrain(X,y, train_size = 0.66, random_state = 42):
+	#Making my own so I can split text!
+	random.seed(random_state)
+	shuffle_X = X
+	shuffle_y = y
+	random.shuffle(shuffle_X)
+	random.shuffle(shuffle_y)
+
+	splitindex = int(math.ceil(len(y)*train_size))
+
+	X_train = shuffle_X[:splitindex]
+	X_test = shuffle_X[splitindex:]
+
+	y_train = shuffle_y[:splitindex]
+	y_test = shuffle_y[splitindex:]
+
+	return X_train, X_test, y_train, y_test
+
 
 def main():
+	numpy.set_printoptions(threshold=numpy.nan)
+
+	#Read Files
 	pickle_name = 'NarrativePickleAgency'
 	texts, scores = readnarratives.readPickle(pickle_name)
 
-	texts_tfidf,vectorizer = tokenize(texts)
+
+	#Split test train
+	X_train, X_test, y_train, y_test = splitTestTrain(
+		texts, scores, 0.66, 42)
+
+	count_vect = CountVectorizer()
+	X_train_count = count_vect.fit_transform(X_train)
+
+	tfidf_transformer = TfidfTransformer()
+	X_train_tfidf = tfidf_transformer.fit_transform(X_train_count)
+
+	X_test_count = count_vect.transform(X_test)
+	X_test_tfidf = tfidf_transformer.transform(X_test_count)
 	
-	X_train, X_test, y_train, y_test = train_test_split(
-		texts_tfidf, scores, test_size=0.33, random_state=42)
+	
+	#Naive Bayes - I'm not sure but this might not work so well
+		#always predicts 1...
+	print ('Naive Bayes')
+	naive_bayes_model = MultinomialNB().fit(X_train_tfidf, y_train)
 
-	naive_bayes_model = MultinomialNB().fit(X_train, y_train)
+	predicted_nb = naive_bayes_model.predict(X_test_tfidf)
+	print('Accuracy: %f' % numpy.mean(predicted_nb == y_test))
 
-	predicted = naive_bayes_model.predict(X_test)
-	print('Accuracy: %f' % numpy.mean(predicted == y_test))
+	print y_test
+	print predicted_nb
 
-	mostInformativeFeatures(naive_bayes_model, vectorizer,[0,1,2,3])
+	mostInformativeFeatures(naive_bayes_model, count_vect,[0,1,2,3],10)
+
+	#Support vector
+	print('Support Vector Machine')
+
+	support_vector_model = SGDClassifier(loss='hinge', penalty='l2',
+		alpha=1e-3, n_iter=5)
+
+	support_vector_model.fit(X_train_tfidf, y_train)
+
+	predicted_svm = support_vector_model.predict(X_test_tfidf)
+	
+	print y_test
+	print predicted_svm
+
+	print('Accuracy: %f' % numpy.mean(predicted_svm == y_test))
+
+	mostInformativeFeatures(support_vector_model, count_vect,[0,1,2,3],10)
+	
+	reliability = scipy.stats.pearsonr(predicted_svm,y_test)
+
+	print (reliability)
+
+
 
 
 if __name__ == '__main__':
--- readme.txt
+++ readme.txt
@@ -2,6 +2,8 @@ This is a project for coding narratives for themes using ML
 
 I'm using narratives 1-130 for investigation, with 130-164 used for testing later
 
+Also, it's worth noting that you can recieve a score of 1.5 for agency, but the data set has none of that...
+
 Preprocessing:
 remove any text in parentheses (because someone added unwanted coded in the narratives)
 remove non-alphabet characters except for spaces
