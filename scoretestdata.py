cd50754e5d316068590ebb1d148f948622c901b8
phiaseitz <sophia.seitz@students.olin.edu>
2015-04-05 19:56:20 -0400

    Working on scoring individual sentences

--- scoretestdata.py
+++ scoretestdata.py
@@ -1,14 +1,40 @@
 import readnarratives
 
 def findOverlap (s1, s2):
-	""" returns the longest common substring from the beginning of sa and sb """
-	def _iter():
-		for a, b in zip(s1, s2):
-			if a == b:
-		    	yield a
-			else:
-		    	return
-	return len(''.join(_iter()))
+	s1_words = s1.split(' ')
+	s2_words = s2.split(' ')
+
+	max_start_j = 0
+	max_end_j = 0
+
+	start_j = 1
+	end_j = 0
+
+	matching = False
+
+	for start_i in range(len(s1_words)):
+		i = start_i
+		for j in range(len(s2_words)):
+			if (s1_words[i]== s2_words[j]) and matching:
+				end_j = j
+				if i < len(s1_words)-1:
+					i = i+1
+			elif (s1_words[i] == s2_words[j]) and not(matching):
+				matching = True
+				start_j = j
+				end_j = j
+				if i < len(s1_words)-1:
+					i = i+1
+			elif matching and not(s1_words[i] == s2_words[j]):
+				matching = False
+				if (end_j-start_j) > (max_end_j-max_start_j):
+					max_start_j = start_j
+					max_end_j =  end_j
+					i = start_i
+		start_j = 0
+		end_j = 0
+		matching = False
+	return (max_end_j-(max_start_j-1))
 
 def getScore(sentence, example_texts, example_scores):
 	max_overlap = 0
@@ -19,13 +45,21 @@ def getScore(sentence, example_texts, example_scores):
 		if (sentence in example_text):
 			return example_scores[i]
 		# calculate overlap with each string 
+		elif example_text in sentence:
+			overlap = len(example_text.split(' '))
+			if overlap > max_overlap:
+				max_overlap = overlap
+				max_overlap_i = i
 		else:
 			overlap = findOverlap(sentence, example_text)
 			if overlap > max_overlap:
 				max_overlap = overlap
 				max_overlap_i = i
+	if max_overlap > 0:
+		return example_scores[max_overlap_i]
+	else:
+		return None
 
-	return example_scores[max_overlap_i]
 		#Otherwise, find the example with the most overlap.
 
 
@@ -38,8 +72,9 @@ def getSentenceScores (narrative_response):
 
 	sentences = scene_text.split('.')
 
+	print example_scores
 	for sentence in sentences:
-		getScore(sentence, example_text, example_scores)	
+		print(getScore(sentence, example_text, example_scores))
 
 
 
@@ -50,6 +85,7 @@ def main():
 	data = readnarratives.readPickle(file_name)
 
 	getSentenceScores(data[0])
+
 	
 if __name__ == '__main__':
 	main()
